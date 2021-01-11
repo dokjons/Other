5a4cb21b6af2bc909ce37fb08e8c50f3f49f1113
Jonas <70606075+dokjons@users.noreply.github.com>
2021-01-11 17:23:07 +0100

    Add files via upload

---
+++ Active Joblog/Joblog-Auto.ps1
@@ -0,0 +1,23 @@
+﻿$username = "YourUsername"
+$password = "YourPassword"
+$ie = New-Object -com InternetExplorer.Application
+$ie.visible = $True
+$ie.navigate("https://job.jobnet.dk/CV/frontpage")
+while($ie.ReadyState -ne 4) {start-sleep -seconds 3}
+$ie.document.getElementById("JobnetUsername").value= "$username"
+$ie.document.getElementById("JobnetPassword").value = "$password"
+$final_login = $ie.document.getElementById('LoginButton')
+$final_login.click()
+start-sleep 5
+$final_login = $ie.document.getElementById('TjobButton')
+$final_login.click()
+start-sleep 5
+$final_login = $ie.document.getElementById('LogoutLink')
+$final_login.click()
+Start-Sleep 5
+$ie.quit()
+
+#Creates the log file and logs when ever the button is pressed.
+$date = Get-Date
+New-Item -Path 'C:\Your\Path\Logs\Joblog.txt' -ItemType File
+Add-Content -Path 'C:\Your\Path\Logs\Joblog.txt' -Value (("#$date") + " - " + "Button pressed and registreted on Jobnet.dk!")
---
+++ Active Joblog/readme.md
@@ -0,0 +1,4 @@
+# Joblog-Auto
+This is a script i made and used when i was looking for a job.
+Where i live you had to go to a website and press a button to show you were actively searching for a job, so i made this script to automatically do it for me.
+I know it's nothing big or special but it was a bit neat at the time and fun to make.
