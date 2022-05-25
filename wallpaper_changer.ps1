84b8df5b9efe144552c98f783376f58348beed26
Jonas <70606075+dokjons@users.noreply.github.com>
2022-05-25 20:33:52 +0200

    Add files via upload

---
+++ wallpaper_changer.ps1
@@ -0,0 +1,25 @@
+﻿$envusr = $env:USERNAME
+$url = Read-Host -Prompt 'Enter your wallpaper url'
+$filename = "C:\Users\$envusr\Downloads\wallpaper.jpg"
+Function Set-WallPaper($Value)
+{
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name wallpaper -Value $Value
+rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+function getfile($url, $filename)
+{
+    $wc = New-Object System.Net.WebClient
+    try
+    {
+        $wc.DownloadFile($url, $filename)
+    }
+    catch [System.Net.WebException]
+    {
+        Write-Host("Kan ikke download $url")
+    }
+    finally
+    {
+        Set-WallPaper -Value "C:\Users\$envusr\Downloads\wallpaper.jpg"
+    }
+}
+getfile $url $filename | Out-Null
